9eed5307ee3bc951ac190cc7da6df236f53b637f
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-31 06:45:01 -0700

    add func acess token create, verify and auth user

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,14 +1,53 @@
-def hash_password(password: str):
-    pass
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+import jwt
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import settings
+from app.core.security import verify_password
+from app.crud.user import get_user_by_username
+from app.models import User
+from app.schemas import TokenData
+
+# ---------------------------- #
+# JWT Token Management
+# ---------------------------- #
+credential_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not verify token, token expired",
+    headers={
+        "WWW-AUTHENTICATE": "Bearer",
+    },
+)
 
 
-def verify_password(plain_password: str, hashed_password: str):
-    pass
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return encoded_jwt
 
 
-def create_access_token(data: dict):
-    pass
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        username: str = payload.get("sub")
+        if not username:
+            raise credential_exception
+        token_data = TokenData(username=username)
+        return token_data
+    except Exception:
+        raise credential_exception
 
 
-def decode_access_token(token: str):
-    pass
+# ---------------------------- #
+# User Authentication
+# ---------------------------- #
+async def authenticate_user(db: AsyncSession, username: str, password: str) -> Optional[User]:
+    user = await get_user_by_username(db, username)
+    if user and verify_password(password, user.hashed_password):
+        return user
+    return None
