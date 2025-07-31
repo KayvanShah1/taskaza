bc61d0ae906c9859774df4caedea25fa44c9c8ab
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-31 16:06:37 -0700

    import fix

--- app/core/auth.py
+++ app/core/auth.py
@@ -8,8 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.config import settings
 from app.core.security import verify_password
 from app.crud.user import get_user_by_username
-from app.models import User
-from app.schemas import TokenData
+from app.models.user import User
+from app.schemas.token import TokenData
 
 # ---------------------------- #
 # JWT Token Management
