5e4e516301c91a7d6011ff165c4ed2bfe4a73caa
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-31 19:06:37 -0700

    added example

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class UserCreate(BaseModel):
     username: str
     password: str
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "examples": [
+                {
+                    "username": "john_doe",
+                    "password": "securepassword123",
+                },
+            ]
+        }
+    )
 
 
 class UserOut(BaseModel):
