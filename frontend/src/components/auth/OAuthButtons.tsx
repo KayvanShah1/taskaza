252d32f09d1a9ebecdf38d3e25bff312d1d19136
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-09-05 01:54:41 -0700

    replace ms with gh

--- frontend/src/components/auth/OAuthButtons.tsx
+++ frontend/src/components/auth/OAuthButtons.tsx
@@ -13,14 +13,7 @@ export function OAuthButtons({ disabled }: { disabled?: boolean }) {
 
 			<div className="grid grid-cols-2 gap-3">
 				<Button type="button" variant="outline" disabled={disabled}>
-					{/* Google */}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="0.98em"
-						height="1em"
-						viewBox="0 0 256 262"
-						aria-hidden="true"
-					>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 262" aria-hidden="true">
 						<path
 							fill="#4285f4"
 							d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
@@ -41,20 +34,14 @@ export function OAuthButtons({ disabled }: { disabled?: boolean }) {
 					<span>Google</span>
 				</Button>
 				<Button type="button" variant="outline" disabled={disabled}>
-					{/* Microsoft */}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1em"
-						height="1em"
-						viewBox="0 0 256 256"
-						aria-hidden="true"
-					>
-						<path fill="#f1511b" d="M121.666 121.666H0V0h121.666z" />
-						<path fill="#80cc28" d="M256 121.666H134.335V0H256z" />
-						<path fill="#00adef" d="M121.663 256.002H0V134.336h121.663z" />
-						<path fill="#fbbc09" d="M256 256.002H134.335V134.336H256z" />
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>GitHub</title>
+						<path
+							fill="currentColor"
+							d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+						/>
 					</svg>
-					<span>Microsoft</span>
+					<span>GitHub</span>
 				</Button>
 			</div>
 		</>
