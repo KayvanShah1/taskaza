bd84a1a43e24ff1724769b1a88c9dac45762a29d
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-09-05 01:14:30 -0700

    move and update nav links into a dedicated file

---
+++ frontend/src/data/nav-links.ts
@@ -0,0 +1,7 @@
+const navLinks = [
+	{ name: "Features", href: "#features", external: false },
+	{ name: "About", href: "#about", external: false },
+	{ name: "API Docs", href: "https://taskaza.onrender.com/redoc", external: true },
+];
+
+export default navLinks;
